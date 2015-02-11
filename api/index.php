e05a17089b88407f0d86ba57e644d3472321d4a7
Eric Johansson (neku) <neku@pomf.se>
2015-02-11 20:50:58 +0100

    placeholdersz

---
+++ api/index.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_GET['d'])){
+	include_once('../includes/core.php');
+	switch($_GET['d']){
+		case 'fetch':
+		break;
+		case 'add':
+		break;
+		case 'change':
+		break;
+		case 'remove':
+		break;
+	}
+}
+?>
